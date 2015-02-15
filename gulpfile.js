1b6616a32a2389d57072bc9b7502a979c11dbe81
flaprogrammer <flaprogrammer@gmail.com>
2015-02-15 22:09:59 +0300

    Исправил ошибку с gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
  
 // include plug-ins
 var jshint = require('gulp-jshint');
-var changed = require('gulp-changed');
+// var changed = require('gulp-changed');
 var imagemin = require('gulp-imagemin');
 
 
@@ -11,7 +11,7 @@ var concat = require('gulp-concat');
 var stripDebug = require('gulp-strip-debug');
 var uglify = require('gulp-uglify');
 
-// include plug-ins
+// // include plug-ins
 var autoprefix = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
 var jade = require('gulp-jade');
@@ -20,7 +20,7 @@ var server = require('gulp-server-livereload');
 
 
 
-// JS hint task
+//JS hint task
 gulp.task('jshint', function() {
   gulp.src('./src/js/*.js')
     .pipe(jshint())
@@ -30,18 +30,18 @@ gulp.task('jshint', function() {
 
 
 gulp.task('imagemin', function() {
-  var imgSrc = './src/img/photos/*',
+  var imgSrc = './src/img/**/*',
       imgDst = './build/img';
  
   gulp.src(imgSrc)
-    .pipe(changed(imgDst))
+    //.pipe(changed(imgDst))
     .pipe(imagemin())
     .pipe(gulp.dest(imgDst));
 });
  
 
  
-// JS concat, strip debugging and minify
+//JS concat, strip debugging and minify
 gulp.task('scripts', function() {
   gulp.src(['./src/js/lib/*.js','./src/js/*.js'])
     .pipe(concat('script.js'))
@@ -50,15 +50,15 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./build/js/'));
 });
 
-gulp.task('htmlpage', function() {
-  var htmlSrc = './src/*.html',
-      htmlDst = './build';
+// gulp.task('htmlpage', function() {
+//   var htmlSrc = './src/*.html',
+//       htmlDst = './build';
  
-  gulp.src(htmlSrc)
-    .pipe(changed(htmlDst))
-    //.pipe(minifyHTML())
-    .pipe(gulp.dest(htmlDst));
-});
+//   gulp.src(htmlSrc)
+//     .pipe(changed(htmlDst))
+//     //.pipe(minifyHTML())
+//     .pipe(gulp.dest(htmlDst));
+// });
 
 
 gulp.task('sass', function () {
@@ -69,13 +69,13 @@ gulp.task('sass', function () {
 });
 
 
-/*gulp.task('webserver', function() {
+gulp.task('webserver', function() {
   gulp.src('build')
     .pipe(server({
       livereload: true,
       defaultFile: 'index.html'
     }));
-});*/
+});
 
 gulp.task('jade', function() {
   var YOUR_LOCALS = {};
@@ -99,17 +99,17 @@ gulp.task('jade', function() {
 });*/
 
 
-gulp.task('default', ['imagemin', 'scripts', 'sass', 'jade'], function() {
+gulp.task('default', ['imagemin','scripts', 'sass', 'jade', 'webserver'], function() {
   // watch for HTML changes
   /*gulp.watch('./src/*.html', function() {
     gulp.run('htmlpage');
   });*/
-  gulp.watch('./src/img/**/*', function() {
-    gulp.run('imagemin');
-  });
+ //  gulp.watch('./src/img/**/*', function() {
+ //    gulp.run('imagemin');
+ //  });
  
   gulp.watch('./src/jade/*.jade', function() {
-    gulp.run('jade');
+     gulp.run('jade');
   });
  
   // watch for JS changes
@@ -118,9 +118,9 @@ gulp.task('default', ['imagemin', 'scripts', 'sass', 'jade'], function() {
   });
  
   // watch for CSS changes
- /* gulp.watch('./src/css/*.css', function() {
-    gulp.run('styles');
-  });*/
+  // gulp.watch('./src/css/*.css', function() {
+  //   gulp.run('styles');
+  // });
   gulp.watch('./src/scss/*.scss', function() {
     gulp.run('sass');
   });
